825ac600d863e2535df21dd6854469329b6caaed
Wayne <wayne@waynecater.com>
2019-04-14 16:38:29 -0500

    initial commit

---
+++ scripts/site.common.js
@@ -0,0 +1,19 @@
+
+//https://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+window.scroll({
+  top: 2500, 
+  left: 0, 
+  behavior: 'smooth'
+});
+
+window.scrollBy({ 
+  top: 100, // can be negative value
+  left: 0, 
+  behavior: 'smooth' 
+});
+
+// document.querySelector('#Music').scrollIntoView({ 
+//    behavior: 'smooth' });
+
+
